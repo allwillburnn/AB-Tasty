f242a54b763d7a546fcef33fdee88dfee9201d9a
m.kazimirau <m.kazimirau@andersenlab.com>
2023-08-29 18:55:57 +0300

    tests added

--- cypress/helpers.ts
+++ cypress/helpers.ts
@@ -6,6 +6,14 @@ class Helpers {
     clickOnElement(element: string): void {
         cy.xpath(element).click();
     }
+
+    checkUrl(expectedUrl: string): void {
+        cy.url().should("equal", expectedUrl);
+    }
+
+    verifyElementIsVisible(element: string): void {
+        cy.xpath(element).should("be.visible");
+    }
 }
 
 export const helpers: Helpers = new Helpers();
